d56dd3e9d891fd90fb3e41bd8f7756259695b0e4
poormonitor <poormonitor@outlook.com>
2021-10-03 07:19:25 -0400

    License Info modified

--- web/template/bs3/js.php
+++ web/template/bs3/js.php
@@ -1,9 +1,10 @@
-<div class=col-sm-2></div>
+<div class=col-sm-1></div>
 <div id=footer class='center col-sm-2'>GPLv2 licensed by HUSTOJ <?php echo date('Y'); ?></div>
+<div id=footer class='center col-sm-2'><a id='footer' href='https://git.oldmonitor.cn/poormonitor/hustoj'>Modified under GPLv3</a></div>
 <div id=footer class='center col-sm-2'>Sponsored by <a id='footer' href='https://oldmonitor.cn'>Johnson Sun</a></div>
 <div id=footer class='center col-sm-2'><a id='footer' href='https://beian.miit.gov.cn/'>浙ICP备2021022182号</a></div>
 <div id=footer class='center col-sm-2'><a id='footer' target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010802011750">浙公网安备 33010802011750号</a></div>
-<div class=col-sm-2></div>
+<div class=col-sm-1></div>
 </br></br>
 <script src="<?php echo $OJ_CDN_URL . $path_fix . "template/$OJ_TEMPLATE/" ?>jquery.min.js"></script>
 <script src="<?php echo $OJ_CDN_URL . $path_fix . "template/$OJ_TEMPLATE/" ?>bootstrap.min.js"></script>
