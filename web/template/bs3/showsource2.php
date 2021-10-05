5c0150b9b4dbff0cfb52b193d26ac1f31a570fad
poormonitor <poormonitor@outlook.com>
2021-10-05 07:31:25 -0400

    Unify Format

--- web/template/bs3/showsource2.php
+++ web/template/bs3/showsource2.php
@@ -8,14 +8,15 @@ if ($brush=='freebasic') $brush='vb';
 if ($brush=='swift') $brush='csharp';
 echo "<pre class=\"brush:".$brush.";\">";
 ob_start();
-echo "/**************************************************************\n";
+echo "\n'''\n";
+echo "=== Submission Info ===\n";
 echo "\tProblem: $sproblem_id\n\tUser: $suser_id\n";
-echo "\tLanguage: ".$language_name[$slanguage]."\n\tResult: ".$judge_result[$sresult]."\n";
-if ($sresult==4){
-echo "\tTime:".$stime." ms\n";
-echo "\tMemory:".$smemory." kb\n";
+echo "\tLanguage: " . $language_name[$slanguage] . "\n\tResult: " . $judge_result[$sresult] . "\n";
+if ($sresult == 4) {
+  echo "\tTime:" . $stime . " ms\n";
+  echo "\tMemory:" . $smemory . " kb\n";
 }
-echo "****************************************************************/\n\n";
+echo "'''";
 $auth=ob_get_contents();
 ob_end_clean();
 echo htmlentities(str_replace("\n\r","\n",$view_source),ENT_QUOTES,"utf-8")."\n".$auth."</pre>";
