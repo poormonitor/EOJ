e80670a9f72e627ef6791b3366d7ef78294c1d7f
poormonitor <poormonitor@outlook.com>
2021-10-14 13:13:11 +0800

    i18n

--- web/template/bs3/user_set_ip.php
+++ web/template/bs3/user_set_ip.php
@@ -91,7 +91,7 @@
         <a href="suspect_list.php?cid=<?php echo $view_cid?>" class="btn btn-warning btn-sm"><?php echo $MSG_IP_VERIFICATION?></a>
         <?php if(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])) {?>
           <a href="user_set_ip.php?cid=<?php echo $view_cid?>" class="btn btn-success btn-sm"><?php echo $MSG_SET_LOGIN_IP?></a>
-          <a target="_blank" href="../../admin/contest_edit.php?cid=<?php echo $view_cid?>" class="btn btn-success btn-sm"><?php echo "EDIT"?></a>
+          <a target="_blank" href="../../admin/contest_edit.php?cid=<?php echo $view_cid?>" class="btn btn-success btn-sm"><?php echo "编辑"?></a>
         <?php } ?>
       </div>
     </div>
