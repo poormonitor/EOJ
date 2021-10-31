e6feada400777df8b145c099902d0b24d0b5f54a
poormonitor <poormonitor@outlook.com>
2021-10-31 10:32:12 +0800

    fix tinymce, update policy file, repair reinfo

--- web/tinymce/tinymce.php
+++ web/tinymce/tinymce.php
@@ -4,19 +4,17 @@
         tinymce.init({
             selector: "#tinymce" + index,
             language: 'zh_CN',
-            plugins: 'image',
-            toolbar: 'image',
             images_upload_url: '/tinymce/upload.php',
             inline: false,
             images_upload_base_path: '',
-            plugins: 'print preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount imagetools textpattern help emoticons autosave autoresize',
+            plugins: 'paste print preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount imagetools textpattern help emoticons autosave autoresize',
             toolbar: 'code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | \
                      formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | \
                      table image media charmap emoticons hr pagebreak insertdatetime print preview | fullscreen | lineheight',
             font_formats: '思源黑体=SourceHanSansCN-Medium',
             height: 650,
             min_height: 400,
-            fontsize_formats: '14px 18px 24px',
+            fontsize_formats: '14px 24px',
             image_class_list: [{
                     title: 'None',
                     value: ''
@@ -31,6 +29,16 @@
             template_mdate_format: '[MDATE: %m/%d/%Y : %H:%M:%S]',
             autosave_ask_before_unload: false,
             toolbar_mode: 'wrap',
+            paste_auto_cleanup_on_paste: true,
+            paste_remove_styles: true,
+            paste_remove_styles_if_webkit: true,
+            paste_strip_class_attributes: true,
+            setup: function(editor) {
+                editor.on('init', function(e) {
+                    this.getBody().style.fontSize = '14px';
+                    this.getBody().style.fontFamily = 'SourceHanSansCN-Medium';
+                });
+            }
         });
     })
 </script>
