057b774f9ef06c6aab24b7523459956455251cf5
poormonitor <poormonitor@outlook.com>
2022-01-28 18:54:37 +0800

    fix

--- web/admin/admin-header.php
+++ web/admin/admin-header.php
@@ -6,7 +6,7 @@ if (file_exists("../lang/$OJ_LANG.php"))
     require_once("../lang/$OJ_LANG.php");
 
 if (!(isset($_SESSION[$OJ_NAME . '_' . 'administrator']) || isset($_SESSION[$OJ_NAME . '_' . 'contest_creator']) || isset($_SESSION[$OJ_NAME . '_' . 'problem_editor']) || isset($_SESSION[$OJ_NAME . '_' . 'password_setter']))) {
-    $view_swal = "{title:'$MSG_NOT_LOGINED',icon:'error'}";
+    $view_swal_params = "{title:'$MSG_NOT_LOGINED',icon:'error'}";
     require("../template/$OJ_TEMPLATE/error.php");
     exit(0);
 }
