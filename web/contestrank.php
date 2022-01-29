7fef9840944442aaa96b2cf9bf9e6bf10c716a91
poormonitor <poormonitor@outlook.com>
2022-01-29 21:02:45 +0800

    memcache config changed

--- web/contestrank.php
+++ web/contestrank.php
@@ -86,47 +86,29 @@ if (!isset($_GET['cid']))
 
 $cid = intval($_GET['cid']);
 
-if ($OJ_MEMCACHE) {
-	$sql = "SELECT `start_time`,`title`,`end_time` FROM `contest` WHERE `contest_id`=$cid";
-	require("./include/memcache.php");
-	$result = mysql_query_cache($sql);
+$sql = "SELECT `start_time`,`title`,`end_time` FROM `contest` WHERE `contest_id`=?";
+$result = mysql_query_cache($sql, $cid);
 
-	if ($result)
-		$rows_cnt = count($result);
-	else
-		$rows_cnt = 0;
-} else {
-	$sql = "SELECT `start_time`,`title`,`end_time` FROM `contest` WHERE `contest_id`=?";
-	$result = pdo_query($sql, $cid);
-
-	if ($result)
-		$rows_cnt = count($result);
-	else
-		$rows_cnt = 0;
-}
+if ($result)
+	$rows_cnt = count($result);
+else
+	$rows_cnt = 0;
 
 $start_time = 0;
 $end_time = 0;
 
 if ($rows_cnt > 0) {
 	//$row=$result[0];
-
-	if ($OJ_MEMCACHE)
-		$row = $result[0];
-	else
-		$row = $result[0];
-
 	$start_time = strtotime($row['start_time']);
 	$end_time = strtotime($row['end_time']);
 	$title = $row['title'];
 }
 
-if (!$OJ_MEMCACHE)
-	if ($start_time == 0) {
-		$view_swal = $MSG_NOT_EXISTED;
-		require("template/error.php");
-		exit(0);
-	}
+if ($start_time == 0) {
+	$view_swal = $MSG_NOT_EXISTED;
+	require("template/error.php");
+	exit(0);
+}
 
 if ($start_time > time()) {
 	$view_swal = $MSG_PRIVATE_WARNING;
@@ -160,29 +142,15 @@ if (time() > $view_lock_time && time() < $end_time + $OJ_RANK_LOCK_DELAY) {
 	$locked_msg = $MSG_RANK_LOCKED;
 }
 
-if ($OJ_MEMCACHE) {
-	$sql = "SELECT count(1) as pbc FROM `contest_problem` WHERE `contest_id`='$cid'";
-	//require("./include/memcache.php");
-	$result = mysql_query_cache($sql);
+$sql = "SELECT count(1) as pbc FROM `contest_problem` WHERE `contest_id`=?";
+$result = mysql_query_cache($sql, $cid);
 
-	if ($result)
-		$rows_cnt = count($result);
-	else
-		$rows_cnt = 0;
-} else {
-	$sql = "SELECT count(1) as pbc FROM `contest_problem` WHERE `contest_id`=?";
-	$result = pdo_query($sql, $cid);
-
-	if ($result)
-		$rows_cnt = count($result);
-	else
-		$rows_cnt = 0;
-}
-
-if ($OJ_MEMCACHE)
-	$row = $result[0];
+if ($result)
+	$rows_cnt = count($result);
 else
-	$row = $result[0];
+	$rows_cnt = 0;
+
+$row = $result[0];
 
 //$row=$result[0];
 $pid_cnt = intval($row['pbc']);
@@ -222,25 +190,14 @@ for ($i = 0; $i < $pid_cnt; $i++) {
 	$first_blood[$i] = "";
 }
 
-if ($OJ_MEMCACHE) {
-	$sql = "select s.num,s.user_id from solution s ,
-	(select num,min(solution_id) minId from solution where contest_id=$cid and result=4 GROUP BY num ) c where s.solution_id = c.minId";
-	$fb = mysql_query_cache($sql);
-
-	if ($fb)
-		$rows_cnt = count($fb);
-	else
-		$rows_cnt = 0;
-} else {
-	$sql = "select s.num,s.user_id from solution s ,
+$sql = "select s.num,s.user_id from solution s ,
 	(select num,min(solution_id) minId from solution where contest_id=? and result=4 GROUP BY num ) c where s.solution_id = c.minId";
-	$fb = pdo_query($sql, $cid);
+$fb = mysql_query_cache($sql, $cid);
 
-	if ($fb)
-		$rows_cnt = count($fb);
-	else
-		$rows_cnt = 0;
-}
+if ($fb)
+	$rows_cnt = count($fb);
+else
+	$rows_cnt = 0;
 
 for ($i = 0; $i < $rows_cnt; $i++) {
 	$row = $fb[$i];
