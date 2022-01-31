f70c0ee569d07149b2f64034a404a55655a6eb34
poormonitor <poormonitor@outlook.com>
2022-01-31 23:44:38 +0800

    fix tinymce table and paste format

--- web/admin/news_edit.php
+++ web/admin/news_edit.php
@@ -1,69 +1,68 @@
 <?php
-require_once("admin-header.php");
 require_once("../include/db_info.inc.php");
 require_once("../include/my_func.inc.php");
-echo "<center><h3>" . $MSG_NEWS . "-" . "Edit" . "</h3></center>";
-?>
 
-<div class="container">
-  <?php
-  if (isset($_POST['news_id'])) {
-    require_once("../include/check_post_key.php");
+if (isset($_POST['news_id'])) {
+  require_once("../include/check_post_key.php");
 
-    $title = $_POST['title'];
-    $content = $_POST['content'];
+  $title = $_POST['title'];
+  $content = $_POST['content'];
 
-    $content = str_replace("<p>", "", $content);
-    $content = str_replace("</p>", "<br />", $content);
-    $content = str_replace(",", "&#44;", $content);
+  $content = str_replace("<p>", "", $content);
+  $content = str_replace("</p>", "<br />", $content);
+  $content = str_replace(",", "&#44;", $content);
 
-    $user_id = $_SESSION[$OJ_NAME . '_' . 'user_id'];
-    $news_id = intval($_POST['news_id']);
+  $user_id = $_SESSION[$OJ_NAME . '_' . 'user_id'];
+  $news_id = intval($_POST['news_id']);
 
-    $sql = "UPDATE `news` SET `title`=?,`time`=now(),`content`=?,user_id=? WHERE `news_id`=?";
-    //echo $sql;
-    pdo_query($sql, $title, $content, $user_id, $news_id);
+  $sql = "UPDATE `news` SET `title`=?,`time`=now(),`content`=?,user_id=? WHERE `news_id`=?";
+  //echo $sql;
+  pdo_query($sql, $title, $content, $user_id, $news_id);
 
-    $sql = "DELETE FROM privilege where rightstr = 'n$news_id'";
+  $sql = "DELETE FROM privilege where rightstr = 'n$news_id'";
+  pdo_query($sql);
+
+  if ($_POST['private'] == '1') {
+    $privilege = $_POST['gid'];
+    $privilege = join(",", $privilege);
+    $sql = "update news set private = 'Y' where news_id = ?";
+    pdo_query($sql, $news_id);
+    $sql = "INSERT INTO privilege (`user_id`,`rightstr`,`valuestr`,`defunct`) SELECT user_id, 'n$news_id','true','N' from users where gid in ($privilege)";
     pdo_query($sql);
-
-    if ($_POST['private'] == '1') {
-      $privilege = $_POST['gid'];
-      $privilege = join(",", $privilege);
-      $sql = "update news set private = 'Y' where news_id = ?";
-      pdo_query($sql, $news_id);
-      $sql = "INSERT INTO privilege (`user_id`,`rightstr`,`valuestr`,`defunct`) SELECT user_id, 'n$news_id','true','N' from users where gid in ($privilege)";
-      pdo_query($sql);
-    } else {
-      $sql = "update news set private = 'N' where news_id = ?";
-      pdo_query($sql, $news_id);
-      $sql = "delete from privilege where rightstr = 'n$news_id'";
-      pdo_query($sql);
-    }
-
-    header("location:news_list.php");
-    exit();
   } else {
-    $news_id = intval($_GET['id']);
-    $sql = "SELECT * FROM `news` WHERE `news_id`=?";
-    $result = pdo_query($sql, $news_id);
-    if (count($result) != 1) {
-      echo "No such News!";
-      exit(0);
-    }
-
-    $row = $result[0];
-
-    $title = htmlentities($row['title'], ENT_QUOTES, "UTF-8");
-    $content = $row['content'];
+    $sql = "update news set private = 'N' where news_id = ?";
+    pdo_query($sql, $news_id);
+    $sql = "delete from privilege where rightstr = 'n$news_id'";
+    pdo_query($sql);
   }
-  ?>
 
+  header("location:news_list.php");
+  exit(0);
+} else {
+  $news_id = intval($_GET['id']);
+  $sql = "SELECT * FROM `news` WHERE `news_id`=?";
+  $result = pdo_query($sql, $news_id);
+  if (count($result) != 1) {
+    echo "No such News!";
+    exit(0);
+  }
+
+  $row = $result[0];
+
+  $title = htmlentities($row['title'], ENT_QUOTES, "UTF-8");
+  $content = $row['content'];
+}
+require_once("admin-header.php");
+?>
+<div class="container">
+  <center>
+    <h3><?php echo  $MSG_NEWS . "-" . "Edit" ?></h3>
+  </center>
   <form method=POST action=news_edit.php>
     <input type=hidden name='news_id' value=<?php echo $news_id ?>>
     <p align=left>
       <label class="col control-label"><?php echo $MSG_TITLE ?></label>
-      <input type=text name=title size=71 value='<?php echo $title ?>'>
+      <input class='form-control' type=text name=title size=71 value='<?php echo $title ?>'>
     </p>
     <p align=left>
       <textarea id="tinymce0" name=content>
@@ -102,7 +101,7 @@ echo "<center><h3>" . $MSG_NEWS . "-" . "Edit" . "</h3></center>";
     </div>
   </form>
 </div><br /><br />
-<?php require_once('../tinymce/tinymce.php'); ?>
 <?php
 require_once("admin-footer.php");
+require_once('../tinymce/tinymce.php');
 ?>
