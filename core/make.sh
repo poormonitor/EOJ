f940c107025616a71ffd5a79f9e3aab64d36b186
poormonitor <poormonitor@outlook.com>
2022-02-01 21:22:56 +0800

    repair sim

--- core/make.sh
+++ core/make.sh
@@ -11,13 +11,13 @@ cd ../sim/sim_3_0_2
 make fresh
 make exes
 chmod +x sim*
-cp sim_c.exe /usr/bin/sim_c
-cp sim_c++.exe /usr/bin/sim_cc
-cp sim_java.exe /usr/bin/sim_java
-cp sim_pasc.exe /usr/bin/sim_pas
-cp sim_text.exe /usr/bin/sim_text
-cp sim_lisp.exe /usr/bin/sim_scm
-cp sim_py.exe /usr/bin/sim_py
+cp sim_c /usr/bin/sim_c
+cp sim_c++ /usr/bin/sim_cc
+cp sim_java /usr/bin/sim_java
+cp sim_pasc /usr/bin/sim_pas
+cp sim_text /usr/bin/sim_text
+cp sim_lisp /usr/bin/sim_scm
+cp sim_py /usr/bin/sim_py
 cd ..
 cp sim.sh /usr/bin
 chmod +x /usr/bin/sim.sh
