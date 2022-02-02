119bc9b3597d591067916a921c388976b475dab1
poormonitor <poormonitor@outlook.com>
2022-02-02 21:15:53 +0800

    fix favicon

--- web/template/balloon_list.php
+++ web/template/balloon_list.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="favicon.ico">
+  <link rel="shortcut icon" href="/favicon.ico">
 
   <title><?php echo $OJ_NAME . $MSG_BALLOON ?></title>
   <?php include("template/css.php"); ?>
--- web/template/contestrank3.php
+++ web/template/contestrank3.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+    <link rel="shortcut icon" href="/favicon.ico">
     <link rel="stylesheet" type="text/css" href="template/scrollboard.css">
     <title><?php echo $OJ_NAME ?></title>
     <?php include("template/css.php"); ?>
