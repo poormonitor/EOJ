04b480e4833732a5e80955a60f1f046808dfb78a
poormonitor <poormonitor@outlook.com>
2022-05-28 20:24:37 +0800

    add lang tag;

--- web/admin/setmsg.php
+++ web/admin/setmsg.php
@@ -46,15 +46,24 @@ require_once("admin-header.php");
             </div>
         </div>
         <div class="form-group">
+            <label class="col-sm-4 control-label"><?php echo $MSG_UPLOAD ?></label>
+            <div class="col-sm-4">
+                <input id="file" type="file">
+            </div>
+            <div class="col-sm-4">
+                <a href="javascript:uploadFile()" class="btn btn-sm btn-primary"><?php echo $MSG_UPLOAD ?></a>
+            </div>
+        </div>
+        <div class="form-group">
             <label class="col-sm-4 control-label"><?php echo $MSG_FLOATING_URL ?></label>
             <div class="col-sm-4">
-                <input name="src" value="<?php echo $content[1] ?>" class="form-control" placeholder="<?php echo $MSG_FLOATING_URL ?>" type="text" required>
+                <input id="src" name="src" value="<?php echo $content[1] ?>" class="form-control" placeholder="<?php echo $MSG_FLOATING_URL ?>" type="text">
             </div>
         </div>
         <div class="form-group">
             <label class="col-sm-4 control-label"><?php echo $MSG_FLOATING_HREF ?></label>
             <div class="col-sm-4">
-                <input name="href" value="<?php echo $content[2] ?>" class="form-control" placeholder="<?php echo $MSG_FLOATING_HREF ?>" type="text" required>
+                <input name="href" value="<?php echo $content[2] ?>" class="form-control" placeholder="<?php echo $MSG_FLOATING_HREF ?>" type="text">
             </div>
         </div>
         <div class="form-group">
@@ -72,6 +81,27 @@ require_once("admin-header.php");
         </div>
     </form>
 </div><br><br>
+
+<?php $_SESSION[$OJ_NAME . '_' . 'uploadkey'] = strtoupper(substr(MD5($_SESSION[$OJ_NAME . '_' . 'user_id'] . rand(0, 9999999)), 0, 10)); ?>
+<script>
+    var uploadkey = '<?php echo $_SESSION[$OJ_NAME . '_' . 'uploadkey'] ?>';
+
+    function uploadFile() {
+        var fd = new FormData();
+        fd.append("uploadkey", uploadkey);
+        fd.append("file", $("input#file").get(0).files[0]);
+        $.ajax({
+            url: "../tinymce/upimg.php",
+            type: "POST",
+            processData: false,
+            contentType: false,
+            data: fd,
+            success: function(data, status) {
+                $("input#src").val(JSON.parse(data).location);
+            }
+        });
+    }
+</script>
 <?php
 require_once("admin-footer.php");
 ?>
