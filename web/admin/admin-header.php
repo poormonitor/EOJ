ff8d24e6258b0dcca0c695b076b84d11c517ae0b
poormonitor <poormonitor@outlook.com>
2022-08-07 22:03:41 +0800

    fix privilege

--- web/admin/admin-header.php
+++ web/admin/admin-header.php
@@ -6,8 +6,8 @@ if (file_exists("../lang/$OJ_LANG.php"))
     require_once("../lang/$OJ_LANG.php");
 
 if (!(isset($_SESSION[$OJ_NAME . '_' . 'administrator']) || isset($_SESSION[$OJ_NAME . '_' . 'contest_creator']) || isset($_SESSION[$OJ_NAME . '_' . 'problem_editor']) || isset($_SESSION[$OJ_NAME . '_' . 'password_setter']))) {
-    $view_swal_params = "{title:'$MSG_NOT_LOGINED',icon:'error'}";
-    $error_location = "../loginpage.php";
+    $view_swal_params = "{title:'$MSG_PRIVILEGE_WARNING',icon:'error'}";
+    $error_location = "../index.php";
     require("../template/error.php");
     exit(0);
 }
