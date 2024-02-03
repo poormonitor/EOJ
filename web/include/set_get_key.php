d5bc45e316581e70de7f9b7a985ec6dd7edd93f7
poormonitor <poormonitor@outlook.com>
2024-02-03 17:52:57 +0800

    fix keys

--- web/include/set_get_key.php
+++ web/include/set_get_key.php
@@ -1,6 +1,9 @@
 <?php
 $get_key_new = strtoupper(substr(MD5($_SESSION[$OJ_NAME . '_' . 'user_id'] . rand(0, 9999999)), 0, 10));
-if (!isset($_SESSION[$OJ_NAME . '_' . 'getkey'])) {
+if (
+    !isset($_SESSION[$OJ_NAME . '_' . 'getkey'])
+    || !is_array($_SESSION[$OJ_NAME . '_' . 'getkey'])
+) {
     $_SESSION[$OJ_NAME . '_' . 'getkey'] = array($get_key_new);
 } else {
     $_SESSION[$OJ_NAME . '_' . 'getkey'] = array_push($_SESSION[$OJ_NAME . '_' . 'getkey'], $get_key_new);
--- web/include/set_post_key.php
+++ web/include/set_post_key.php
@@ -1,6 +1,9 @@
 <?php
 $post_key_new = strtoupper(substr(MD5($_SESSION[$OJ_NAME . '_' . 'user_id'] . rand(0, 9999999)), 0, 10));
-if (!isset($_SESSION[$OJ_NAME . '_' . 'postkey'])) {
+if (
+    !isset($_SESSION[$OJ_NAME . '_' . 'postkey'])
+    || !is_array($_SESSION[$OJ_NAME . '_' . 'postkey'])
+) {
     $_SESSION[$OJ_NAME . '_' . 'postkey'] = array($post_key_new);
 } else {
     $_SESSION[$OJ_NAME . '_' . 'postkey'] = array_push($_SESSION[$OJ_NAME . '_' . 'postkey'], $post_key_new);
