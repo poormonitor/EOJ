b541d3b712009ec3939097118a68f7cbe9a169a4
poormonitor <poormonitor@outlook.com>
2024-02-07 13:33:16 +0800

    remove spider record

--- web/include/online.php
+++ web/include/online.php
@@ -82,6 +82,10 @@ class online
 			$this->ua = htmlentities($_SESSION[$OJ_NAME . '_' . 'user_id'], ENT_QUOTES, "UTF-8");
 		else
 			$this->ua = "guest";
+		$user_agent = lowercase($_SERVER['HTTP_USER_AGENT'] || "");
+		if (strstr($user_agent, "bot") || strstr($user_agent, "spider")) {
+			return;
+		}
 		$this->ua .= "@" . htmlentities($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, "UTF-8");
 		$this->uri = ($_SERVER['PHP_SELF']);
 		if (isset($_SERVER['HTTP_REFERER'])) {
